f738fbeaae0a625079edf47894dd58e4f638fa84
blackdynamo <d16345401@gmail.com>
2016-09-21 16:14:06 -0600

    new build version

--- build/index.js
+++ build/index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   dailyProduction: require("./daily-production"),
+  load: require("./load"),
   speed: require("./speed"),
   temperature: require("./temperature"),
   torque: require("./torque"),
