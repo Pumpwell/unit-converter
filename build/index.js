60a2456643ced2bfc0b31fc1d30a15755bce3a9b
blackdynamo <d16345401@gmail.com>
2016-10-17 12:44:08 -0600

    adding length to the build

--- build/index.js
+++ build/index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   dailyProduction: require("./daily-production"),
+  length: require("./length"),
   load: require("./load"),
   position: require("./position"),
   speed: require("./speed"),
