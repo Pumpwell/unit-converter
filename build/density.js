415aea5a677cbac944d46a1c63c41fb1f3a515a5
Matthew Schmidt <mschmidt@pumpwell.ca>
2017-01-11 12:42:49 -0700

    adding density to build
    Signed-off-by: Matthew Schmidt <mschmidt@pumpwell.ca>

---
+++ build/density.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var types = require("./types");
+
+module.exports = {
+  convert: function convert(_ref) {
+    var _ref$from = _ref.from,
+        from = _ref$from === undefined ? types.metric : _ref$from,
+        _ref$to = _ref.to,
+        to = _ref$to === undefined ? types.metric : _ref$to,
+        _ref$value = _ref.value,
+        value = _ref$value === undefined ? 0 : _ref$value;
+
+    // const conversion = 16.018463;
+    var conversion = 0.062428;
+    from = from.toLocaleLowerCase();
+    to = to.toLocaleLowerCase();
+
+    if (from === to) return value;
+    if (from === types.metric && to === types.imperial) return value * conversion;
+    if (from === types.imperial && to === types.metric) return value / conversion;
+
+    throw new Error("Conversion from " + from + " to " + to + " unknown");
+  }
+};
