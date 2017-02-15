8e7db96131598e8cf3a6caf91b391ff14a6c0356
Matthew Schmidt <mschmidt@pumpwell.ca>
2017-02-15 15:52:45 -0700

    defaulting gas production to m3/d
    Signed-off-by: Matthew Schmidt <mschmidt@pumpwell.ca>

--- build/daily-gas-production.js
+++ build/daily-gas-production.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var types = require("./types");
-
 var CUBIC_METER_PER_DAY = "m3/d";
 var THOUSAND_CUBIC_METERS_PER_DAY = "e3m3/d";
 var CUBIC_FEET_PER_DAY = "cf/d";
@@ -12,10 +10,9 @@ module.exports = {
     var _ref$from = _ref.from,
         from = _ref$from === undefined ? CUBIC_METER_PER_DAY : _ref$from,
         _ref$to = _ref.to,
-        to = _ref$to === undefined ? THOUSAND_CUBIC_METERS_PER_DAY : _ref$to,
+        to = _ref$to === undefined ? CUBIC_METER_PER_DAY : _ref$to,
         _ref$value = _ref.value,
         value = _ref$value === undefined ? 0 : _ref$value;
-
 
     if (from === to) return value;
 
