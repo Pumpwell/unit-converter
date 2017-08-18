d580b7a1815f4af5ebbd6e0214a20f808a3e9250
blackdynamo <d16345401@gmail.com>
2017-08-18 10:02:13 -0600

    added depth and height conversions

---
+++ build/depth.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var convert = require("./convert");
+
+module.exports = {
+  convert: convert({
+    ft: "ft",
+    ftkb: "ft",
+    imperial: "ft",
+    m: "m",
+    metric: "m",
+    mkb: "m"
+  })
+};
